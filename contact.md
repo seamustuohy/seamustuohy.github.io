54a8d3d07273f781b187809f0560eaa969cf5801
seamus tuohy <code@seamustuohy.com>
2016-11-20 08:52:59 -0500

    Adding gpg email support

---
+++ contact.md
@@ -0,0 +1,15 @@
+---
+layout: page
+title: Contact
+permalink: /contact/
+---
+
+## Contact
+
+You can contact me via e-mail at (s2e@prudentinnovation.org).
+
+If you contact me via email, I encourage you to use GPG/PGP encryption. If you already use PGP e-mail please [download my public key.]({{ site.baseurl }}PGP)
+
+If you don't know how to use PGP you can use the field below to encrypt your message to me before sending it.
+
+{% include gpg_email.html %}
---
+++ js/contact_me.js
@@ -0,0 +1,122 @@
+var pubkey =
+    ['-----BEGIN PGP PUBLIC KEY BLOCK-----',
+     'Version: GnuPG v1',
+     '',
+     'mQINBFY/0XwBEACwRq/jFGOZ7ZpmPNNtUwSMsdWPSnBGMmlhjCsSEyoznVaB8/W2',
+     'SlAvnUC/VAXTvW0cbaPw8uiMbJzndZKpYDbG7LkXnNQAGPLuXdqfdUlt4IXk0A9A',
+     '/sdIVjkpkRZb0/b7zgQ+DO66UO/ltcHU/8aBeH4QQCehK0/X27oRjzVj2ni9KMj0',
+     'Pzy2EUmgyhtY/YLOlhwULcI5SYk4z5xhdhkJOfJDeze6MOn/69QibvpKT0gJqXI/',
+     'PyTCgviWscrjVKvk6xG/93eSkBFk1mhbjXaSNK8Rafp4mowQiZllMJC2glNCbVpu',
+     'hOCny+Kc4JtvRTItyb6LiF5XUe3xzZx8D0DvHP81JMq3Dk2O5FY8aoiOfgBN7rb+',
+     '8G5DUrgRaAr+KH3tisDi5x004ZMMPiW3WwTv9sZet9jRW6uKXIZdivscPmrhymGt',
+     '3ifjmeeB+ppj8egc/HIQCWdMv7Amczt+5XrIlkMhDPhYbmM4qd2rAgrbuGaz9pHg',
+     '3g6F47FjfFrxwpX8JRbQc29T1XvijOgsGKFYG4AgPW5ahBJXx/OCehoouU2/kmeB',
+     'ZCr3OFp9dO38e5lCTxQZPwbkqCZEoXJeNOeH0Vl4HhahDPNdrKxcItAazQxDxgdR',
+     'I06FBn5YitJauBybP+DRCw8fGhlQXOMTf9/FBRNeDcAPUx2PSWki/1hAcwARAQAB',
+     'tCJzZWFtdXMgdHVvaHkgPHMyZUBzZWFtdXN0dW9oeS5jb20+iQI9BBMBCAAnBQJW',
+     'P9r2AhsDBQkDwmcABQsJCAcDBRUKCQgLBRYCAwEAAh4BAheAAAoJELYZPsc88Hqn',
+     '/gAQAKLocwx+X2ErIPuibZmYuw1vls8TJy8eKRUIZXTVbXhWvROuGA+LPrVVLh3Y',
+     'XEYn7l4pPpYk+5d3IdGSOQ+ku8kaSWsOMPWecQHuhhw4qzWqh0A2dttPlIMOmLtW',
+     'SZOxKkRVItq5+9N4+8+MQEK+MNn6pEEJ441tLMxrRU3mA3aQw5OeRhRTWvJg0jkW',
+     'DKX7+tfZ9CR7Qhm5vVcepAozXfcncHASro6GhzkymmF6fC0qYnKKaNvFwjpM4Lpw',
+     'lPwsRQeHpUu73mlekvq+PVhWOffjdAKfoMSMjkhkjK7Vi7IaFFw2jQAxOsLmw2j3',
+     'qGWcsCwYNjGDxT8Ra6NR0CIOTM/Ngs02NdcnX6uIZmD0cI6wQTv9Y9kEeGQfLfue',
+     '1CSzHOF7ib9HDPY9GLq1l1T9tpTXidQlmOGYx3l9Mw1+3A+8qvulsnMJIPhNE7mK',
+     '95Ucl/TRQQvRQ3di+lNhAjHqGqU615gi3qmJi/8NNEBWy0rfNabBpvU/KWNN0zcG',
+     '+KABU5TWmsyDFNUdAZNWmll5CU6Cm5jAo42dyep4M3DbEKjAz6TihvsOWzLPXd+D',
+     'x0w3Whtyz4bIgoXT5qFocJEb+bQA9c7M1/DyNOyMHNqTNfGZH4+dlEL/zp0AWk15',
+     'C1pQkuSKnCDnuPdrjbvXi5stR5R+vqkxVULEUVoj2aalsn+atCNzZWFtdXMgdHVv',
+     'aHkgPHN0dW9oeUBpbnRlcm5ld3Mub3JnPokCQQQTAQgAKwIbAwUJA8JnAAYLCQgH',
+     'AwIGFQgCCQoLBBYCAwECHgECF4AFAlZCCU4CGQEACgkQthk+xzzweqfNng/8D06d',
+     'cq5Fbb6iEp43eDfs2FAAQZQe5aChLejiVb9J3jBeIIGrHIHJNxYz3i39zrcUBbBJ',
+     'gc7Mmq++pPbLrDxYXqdvYh4hJ6/F0O+TpKU30qlAcuPVPkYq1L307huMGT5ryiX1',
+     '+hCEq6QuVM2Z3QekdVZg64thuVYlbXX5XfhNWuCRXHgLoSsT+f5+OZvHKc3ut94V',
+     'z0LUdGS+h+ODl1s+fc8+Sq5FRS0HzLmNb6i13rvBjv57SX6Ocg+iv9lGx+DUGYVU',
+     '/0RI1gYuP9VOl8IUV0nXDfBSJZoSthF3w5Wk+tWvJ7YRzbzWtYoN53shjsipljTb',
+     'pnAofrqoIZeFUxJucS/tAPynphvks3cxb5M4SZHXUSz3GtKFrZfzKEskRdEWIHHy',
+     'Opw0zPfYAbB1dhv030ixZW4AYsF7YatxHzD+/2az+Gro03exrgOdA1w51jyc4OhR',
+     'mVaNvt2IbxuZdhkkX0PQhIx4wXu5Ke/SjsPJVLcH0if6yp+sjKpqGUb4RgsMInNf',
+     'j4PlpthvQIwFE/Wzei4FAFljwyTXRhQh9dKr5/d9bMGXKLWwraUuE11hQP7jIR+K',
+     'AcIqWbyACkgFIeCdL3wrYyWLLdDND+Sj7w5wfZqiPdZAQvD8jkWLkqCmhNKcvy7S',
+     '6Y/o6k8jwMVl2t5c0bE91rBloJapblZIXogT+m2JAhwEEAEIAAYFAlY/1rAACgkQ',
+     'D2JMCtpyVpXpSQ/+PIZbKRlRGSAM6+ibI05leAGOrgJ8Zlkgyy8XjMcAqx/Qx6kT',
+     'q90XzfGveqAIWkG1ebc3OjyKpGIxnTEbzIH6dnWHxkBFxFJK63dQ5vMti2sqAykQ',
+     'xKkqTlSDPkhr1nupcU8yzb5WIZFb83zn+EgwjMpuTQZzcqjYV85xnpE+p3/xwktc',
+     'YQeadbbdE1gqI0HcrOEogrtFGYUbFQs8uP432q+afU/bzTnk5SUcJxkz3/7ssXB4',
+     'tq99NgzQhdgNar+SRb4K2Je5zb34m1d8AOtQ1RAlDutupK/s/2GgoKMFTCSOfL8G',
+     'vRdXiNtY5X8QUD9j8+gdI6Xli528SoYvEGxPX4xkyl2UzNBD3IWcYV0xZVCNMXmB',
+     'GT6SlUEowabIcPyD97OuffM/wjy6Qa2nbrugfdi8K+5tZcNkZ/3dBJh+qPX2yRu4',
+     'CYAeTD/TwDXOzsqGNZk7rEA5NAaXQ9VgQPej1CIDndLa86fX8l07IBjWf0x5oucF',
+     'vkhxp9598szFEMNuVCTTyJJINuC8U8fNdFsyepQhZ3e0lF2wjT0altStfPSSqlKV',
+     'u+av/2veJS1YieQoRIoh1I5/LApSNi6LLj+BML4a67fIlZKjl2HXcNQZVJSgeg3e',
+     'tOKIltT1seYwL11omD9gbBaYFntLzFIM9mxzCU3LzrYUYfuwqeaKY0OyCO2JAj4E',
+     'EwECACgFAlY/0XwCGwMFCQPCZwAGCwkIBwMCBhUIAgkKCwQWAgMBAh4BAheAAAoJ',
+     'ELYZPsc88HqnvkcP/1VND3ADkYS5UaT8cVmEIVdrcbBIs8TxKeczjyaUqXsfoLS3',
+     'CuxpWXireVg9YHEErb3a+QIoySYStALq0/cp82KCpC3/KiNhVLQx/UM3O59T3CI3',
+     'nk9nJTxtGmDl2T8xKOsmgvqMEaoR/rTA0ya4QuDzkybSwyCbDHliHdkNu1tQTkyj',
+     '7uNc1eWbpVUGa9oWS2UqKl9xObZWZM/KT7dPTRuHkSge6m72vf6LFmFeGz/g9yDq',
+     'AywVlyyfoEU5QTaIIX8YQmvjY8mrLL+FUBq8Hhbj2OuGQGjZTLIndtM4Oe62gljl',
+     'mam0QpMKZpfrX/2481Zqpx58FlyArpuQfGLJ79Hx85nUa843aMfhO9iC5cU/MsQ0',
+     '4vXQ7rn+8ozexGhS/dCwFu7gCGryAV0LFcrtsVwYuuHhZTRy2fqeE9E6rdtfGFXG',
+     'MU9A8ib6j4lGxWwN1t2vBEL3k40hTWljBWVkElITm1chzlbBWu3tAFWqbSRR/ec5',
+     'RCF5S82O3Lb9MRitExczDlTP5+TI2qna9wLzyrq+KpE/Ua3s69oeCKKfQymW3S+g',
+     'jOs3bUGa/GqLfxoCdnKTXnLZQBIv8ZeGlQPVnnBOn4qH1OmhkGxWWAfFdazGZOqc',
+     '7siOtUYhqAxzrfOnrESHsPhhQtbqm9f6ABN23I2o31sYHijfTK1b4yse8/tvuQIN',
+     'BFY/0XwBEAC78HrSAZldj18ATBtHM1C7238jT3KCOzu0YY6HPEsLDxeCbMQ16yTP',
+     'SOmekyw+ATgDKPHjIHVrxtJeZE60xWNJtCgsOyfQ2qL+OsGFlRaAM1U+SWU5yx66',
+     'QsCx6qphFYOoO15y2HM7nljNIZShuWS1+eJ5Ri1egr5P8/HuGnZrdqlAXh4xAJLG',
+     'kfE6hoF0J15HSaI6vrnOwLijGffoPbMHMlRIHhkxV+aWu6OLbi4hA47mtXtN4WdX',
+     'IzZeLD57bEZmPlPZdjN6rYemcN7L9bNqBddNG82wom2wczYmCXUHfvIGd3182Zrh',
+     'kuJ1+9HBHsxMI3R/iCRhaUBf/DvRf2u47ADR8Cp9POMPePMj6k8xDQONpTZyDwlN',
+     'rVT5SKZbYesFvGXY1RdZ71MubSF2fZG4mhaAHVwsVTYjCcXyYq1rInnLDtE2WSVD',
+     '41nR14P3mu2qxwh9aImZ3oAUdndxT676xhGIA4zvZagZD36e7bUtR7BzLY2kYg1d',
+     'YaZrRmhhyTuQ51ziHdW8oc5x9o5ZmiqiXIuFDvrs+PjtmgBxQO3MmJjKCLYFN2IC',
+     'eJz9EsyVF8tLpAVcBr2T796jCQODvQBlVwpcrlLQjOuaxK36yAhRvw1IQFyGMV2Z',
+     '9EZoVBtQWN9Ba87AwQkl1OsEWS3hBJJn4K8Pu4I5cMMZawI/Wc+RuwARAQABiQIl',
+     'BBgBAgAPBQJWP9F8AhsMBQkDwmcAAAoJELYZPsc88Hqn/1IQAKuvQs2lddoQnji5',
+     'LbU2Kv9ooqPPMeWVh76RZL6mpQuaGjrvz/OgYCPjEr+59iRLcdJPVF0HF/U8vwqu',
+     'c4wC5EkP8cZrQP/7He8m6wa/r+04uGFZNYPObejUvZnHz4VFJr+3XEmjsq5+D8Z3',
+     'uzBXVRk89sDM1sOzfqxeLDU2aQvcwAbipg7uW1sZJOZXAB5xk5xlFZ+g5JbkzbO3',
+     'tzPCu0IyCxx19/cK/GRUOYCBJl5F11FCgq3kHKJGfukWDJKvO90T+Puv6/Wnd7Gf',
+     'rrIC9sDRmCtt/kkCyFUkWQWXEzrV1SrH8pnbRUQimeBpViQRt3C9s4zrNTwZfxlM',
+     'e/ug9f0sL4W0u4k6dL1qB/1EZH2G+E6GH+AYjV9u7uU7rW6ZCUoVKrXONhIxleDP',
+     'iHlxGw79D2woJYcHmvfUqvMfRg8Lkb/qCdc30k/EO0ANBlbx5ZgTg3OVu+XFrqT0',
+     'x/y7KJehJjxgeJJX7gekmgVKZI+pJ30i/V/gKi2d4hNfb9h5ovKid56xEDrCL6Lx',
+     'eYWBqIKjgC79OHdQyi2Se7TNi8zEZvNS2jMekd0DRcgFHjEAKilhhgdo3wiQnPs2',
+     'jdiYRGNFm99lz1+JzjjJxOclBP59RpHvwEBwB+tG7kVmMeYdg2LH5YQ3L3oghNjM',
+     'AIi/rKgF3CEcXLs4uRo8nTuMxZOW',
+     '=a3jg',
+     '-----END PGP PUBLIC KEY BLOCK-----'].join('\n');
+
+function sendEmail(encrypted) {
+    var email="hello@seamustuohy.com";
+    var subject="Contacting you from your website";
+    var mailto_link='mailto:'+email+'?subject='+subject+'&body='+encrypted;
+    win=window.open(mailto_link,'emailWin');
+}
+
+function showEncrypted(encrypted) {
+    var code = document.createElement("pre");
+    var note_text = document.createTextNode(encrypted);
+    code.appendChild(note_text);
+    var parent = document.getElementById('note_viewer');
+    parent.innerHTML = '';
+    parent.appendChild(code);
+}
+
+function getEncryptedNote(output_loc) {
+    var options, encrypted_note;
+    var raw_note = document.getElementById('note').value;
+    options = {
+        data: raw_note,
+        publicKeys: openpgp.key.readArmored(pubkey).keys,
+    };
+    openpgp.encrypt(options).then(function(ciphertext) {
+        //encrypted_note = ciphertext.data.replace(/(?:\r\n|\r|\n)/g, '\n');
+        if (output_loc == 'show') {
+            showEncrypted(ciphertext.data);
+        } else {
+            sendEmail(ciphertext.data);
+        }
+    });
+}
